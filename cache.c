3f0bcf56b88a2cac569e1fec2fc0e5ee9cdc37cf
Milagros Cruz <mivircruz@gmail.com>
2019-11-10 16:38:42 -0300

    init, get missrate y getoffset

--- cache.c
+++ cache.c
@@ -14,9 +14,12 @@
 #define SIZE_OFFSET_BITS 6
 #define SIZE_TAG_BITS 5
 
+#define BINARY_MASK 32768 //mask = [1000 0000 0000 0000]
 
 #define MASK_TAG 0x0000FF0000000000
 #define MASK_VALID 0xFF00000000000000
+#define MASK_OFFSET 0x003F
+
 /* ******************************************************************
  *             STRUCT CACHE ADT DEFINITION
  * *****************************************************************/
@@ -36,20 +39,58 @@ struct cache
     //Metadata
     //First byte: Valid
     //Second byte: Dirty
-    //Third bytes: Tag
+    //Third byte: Tag
     //Rest : counter
     unsigned char cache_metadata[CACHE_SIZE / (BLOCK_SIZE * CACHE_WAYS)][CACHE_METADATA * CACHE_WAYS];
 
     //Miss rate
-    size_t MR;
+    float MR;
 
-} CACHE;
+} cache_t;
+
+cache_t CACHE; 
 
 
 /* ******************************************************************
- *             CACHE FUNCTIONS
+ *                    AUXILIAR FUNCTIONS
  * *****************************************************************/
 
+unsigned int binary(unsigned int num) {
+    unsigned int binary[64];
+    unsigned int mask = BINARY_MASK;
+
+    while(mask > 0) {
+   if((num & mask) == 0 )
+        strncat(binary, "0", 1);
+   else
+        strncat(binary, "1", 1);
+    mask = mask >> 1 ;  // Right Shift
+   }
+   return binary;
+}
+
+/* ******************************************************************
+ *                      CACHE FUNCTIONS
+ * *****************************************************************/
+
+//Initializes:
+//Main memory to 0
+//Cache lines as invalid
+//Miss rate to 0
+void init(void){
+    memset(MAIN_MEMORY, 0, sizeof(MAIN_MEMORY)); //Main memory initialized to 0
+    memset(CACHE.cache_metadata, 0, sizeof(CACHE.cache_metadata)); //Dirty, Tag and counter initialized to 0
+    for(size_t i = 0; i < CACHE_SIZE / (BLOCK_SIZE * CACHE_WAYS); i++) {
+        for(size_t j = 0; j < CACHE_METADATA * CACHE_WAYS; j += CACHE_METADATA)
+            CACHE.cache_metadata[i][j] = 1; //Valid initialized to 1
+    }
+}
+
+//Returns the byte's offset of the memory block to which the address maps
+unsigned int get_offset (unsigned int address){
+    unsigned int binary = binary(address);
+    return binary & MASK_OFFSET;
+}
 
 //Reads the block 'blocknum' from memory and 
 //stores it on the specified way and set on cache memory
@@ -119,4 +160,14 @@ unsigned int get_tag(unsigned int address)
     unsigned int mask = pow(2,SIZE_TAG_BITS) - 1;  
     unsigned int tag = (address >> (SIZE_SET_BITS + SIZE_OFFSET_BITS)) & mask;
     return tag;
+}
+
+//Returns miss rate
+float get_miss_rate(void) {
+    return CACHE.MR;
+}
+
+//Releases ADT cache
+void release(void){
+    //do nothing
 }
